582dbb22e0b276c09782c74bb723b17fc30570d8
Jaroslav Drobotenko <icaasyww@gmail.com>
2025-05-28 12:03:12 +0300

    feat: implement Dark theme color scheme

---
+++ src/theme/DarkTheme.cpp
@@ -0,0 +1,7 @@
+#include "../../include/theme/DarkTheme.h"
+
+COLORREF DarkTheme::getBackgroundColor()  { return RGB(40, 40, 40); }
+COLORREF DarkTheme::getTextColor()  { return RGB(255, 255, 255); }
+COLORREF DarkTheme::getHighlightColor()  { return RGB(70, 70, 70); }
+COLORREF DarkTheme::getFixedNumberColor()  { return RGB(100, 149, 237); }
+COLORREF DarkTheme::getUserNumberColor()  { return RGB(144, 238, 144); }
