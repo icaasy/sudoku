36428543969d5359d5e644b0e2d630cb1370623f
Jaroslav Drobotenko <icaasyww@gmail.com>
2025-05-28 12:33:16 +0300

    feat: implement Ocean theme color palette

---
+++ src/theme/OceanTheme.cpp
@@ -0,0 +1,7 @@
+#include "../../include/theme/OceanTheme.h"
+
+COLORREF OceanTheme::getBackgroundColor() { return RGB(175, 238, 238); }
+COLORREF OceanTheme::getTextColor() { return RGB(0, 0, 128); }
+COLORREF OceanTheme::getHighlightColor() { return RGB(95, 158, 160); }
+COLORREF OceanTheme::getFixedNumberColor() { return RGB(25, 25, 112); }
+COLORREF OceanTheme::getUserNumberColor() { return RGB(0, 139, 139); }
