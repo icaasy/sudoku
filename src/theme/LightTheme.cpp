3c9d3b9ce47a34346c0cd51ccb45ae1eb6267f69
Jaroslav Drobotenko <icaasyww@gmail.com>
2025-05-28 12:27:22 +0300

    feat: implement Light theme color scheme

---
+++ src/theme/LightTheme.cpp
@@ -0,0 +1,7 @@
+#include "../../include/theme/LightTheme.h"
+
+COLORREF getBackgroundColor()  { return RGB(255, 255, 255); }
+COLORREF getTextColor()  { return RGB(0, 0, 0); }
+COLORREF getHighlightColor()  { return RGB(173, 216, 230); }
+COLORREF getFixedNumberColor()  { return RGB(0, 0, 139); }
+COLORREF getUserNumberColor()  { return RGB(0, 100, 0); }
